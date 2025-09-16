2a310e0247c18a287cdb6933eefbd78cae8276d3
King182008 <cte-xaiden.morey@sllboces.org>
2025-09-16 13:52:03 +0000

    song

---
+++ printExpressions.py
@@ -0,0 +1,16 @@
+'''
+class: CIS Seniors 25-26
+Name: Xaiden Morey
+Assignment: Printing Expressions
+'''
+
+print("Hello, my name is Xaiden.")
+
+print("My favorite song!")
+print("--------------")
+print()
+print("... the first 4 lines of your song.")
+print("One day you opened up your eyes inside of you")
+print("Inside a world inside a universe you didn't get to choose")
+print("You didn't get to pick the rules or pick the past or set the pace")
+print("Or cast the cast and crew")
