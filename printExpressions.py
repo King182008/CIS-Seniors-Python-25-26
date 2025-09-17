5782d66627340450663a029d50ed5b6e6d61855a
King182008 <cte-xaiden.morey@sllboces.org>
2025-09-17 14:07:50 +0000

    variables

--- printExpressions.py
+++ printExpressions.py
@@ -14,3 +14,7 @@ print("One day you opened up your eyes inside of you")
 print("Inside a world inside a universe you didn't get to choose")
 print("You didn't get to pick the rules or pick the past or set the pace")
 print("Or cast the cast and crew")
+
+print()
+print(200 + 400)
+print("Good morning," + " my name is Xaiden")
---
+++ variables.py
@@ -0,0 +1,28 @@
+'''
+class: CIS Seniors 25-26
+Name: Xaiden Morey
+Assignment: Variable basics
+
+Date: 9/17/2025
+'''
+# This is a single line comment
+# Variables can hold all data types
+myInt = 17
+print(myInt)
+
+myString = " Hello, I love silksong"
+print("My name is Xaiden." + myString)
+
+myBoolean = True
+print(myBoolean)
+
+myFloat = 99.8
+print(myFloat)
+
+myAge = myInt
+print("My age is:", myAge)
+print(myInt + myAge) # 34
+
+print(myFloat) #99.8
+myFloat = 100.00    #Reassigned the variable
+print(myFloat) #100.00
