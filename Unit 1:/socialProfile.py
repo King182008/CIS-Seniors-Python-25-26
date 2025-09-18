4416cf9d53aa5512d83aa225cb6bf6cab13f7eb6
King182008 <cte-xaiden.morey@sllboces.org>
2025-09-18 14:37:00 +0000

    Thursday

---
+++ Unit 1:/socialProfile.py
@@ -0,0 +1,29 @@
+"""
+File Name: Social profile
+Name: Xaiden Morey
+Course: Computer and Info Science 
+Date: 9/18/2025
+"""
+
+userName = "SpecialGuy14"
+bio = "Hi i have autism and I really love to play with my monster trucks"
+favoriteQuote = "Arizona Tea for Life gang"
+followers = 8456789
+following = 2
+posts = 800
+averageLikesPerPost = 150871
+engagmentRate = 72
+verifiedAccount = True
+privateProfile = False
+
+print("\n\n\n")
+print("Name: " + userName)
+print("Bio: " + bio)
+print("Quote: " + favoriteQuote)
+print("Followers: ", followers)
+print("Following: ", following)
+print("Posts: ", posts)
+print("Average likes per post: ", averageLikesPerPost)
+print("Engagment Rate: ", engagmentRate)
+print("Verified: ", verifiedAccount)
+print("Private Profile: ", privateProfile)
