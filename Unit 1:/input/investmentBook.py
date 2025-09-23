1650968d47b4271ee07b3db68bd18f7586fdbd05
King182008 <cte-xaiden.morey@sllboces.org>
2025-09-23 14:51:57 +0000

    Tuesday pt3

---
+++ Unit 1:/input/investmentBook.py
@@ -0,0 +1,44 @@
+'''
+File: Investment
+Name: Xaiden Morey
+Class: CIS Seniors
+Date: 9/22/2025
+
+Compute Investment Report
+
+1. The inputs are:
+    Starting investment amount
+    number of yaers
+    Interest rate (an Int percent)
+
+2. The report is displayed in tabular form with a header
+3. Computations and outputs:
+    for each year of the investment compute the interest and add it to the investment
+    print a formatted row of results for that year
+
+4. The ending investment and interest you have paid in total are also displayed
+'''
+
+print("\n\n")
+print("=" * 100)
+print("My Investment Tracker")
+print("=" * 100)
+
+# Accept the inputs
+startBalance = float(input("Enter the investment amount: "))
+years = int(input("Enter the numbers of year: "))
+rate = int(input("Enter the rate as a %: "))
+
+# Convert the rate to a decimal
+rate = rate / 100
+
+# Initialize the accumulator for the interest
+totalInterest = 0.0
+
+# Display the header for the table
+
+
+# Compute and display the results for each year
+
+
+# Display the totals for the period
