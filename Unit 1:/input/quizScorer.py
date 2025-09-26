5c0076c9f2a578cfb1bfd7f418ae697d79f3337e
King182008 <cte-xaiden.morey@sllboces.org>
2025-09-26 13:20:14 +0000

    quiz comments

--- Unit 1:/input/quizScorer.py
+++ Unit 1:/input/quizScorer.py
@@ -9,10 +9,12 @@ Date: 9/25/2025
 name = input("What is your name? ")
 questions = int(input("How many questions are on the quiz"))
 
+# Initialize lists and counters
 studentAnswers = []
 quizAnswers = []
 correctAnswers = 0
 
+# Get students answers and correct answers
 print("What are the students answers? (1 for True 0 for False)")
 for count in range(1, questions + 1):
     sAnswer = list(input("Question" + str(count) + ":"))
@@ -23,6 +25,7 @@ for count in range(1, questions + 1):
     qAnswer = list(input("Quiz Answer " + str(count) + ":"))
     quizAnswers.append(qAnswer)
 
+# Find the number or correct answers 
 print("=" * 40)
 for count in range(1, questions + 1):
     if studentAnswers[count - 1] == quizAnswers[count - 1]:
@@ -31,6 +34,7 @@ for count in range(1, questions + 1):
     elif studentAnswers[count - 1] != quizAnswers[count - 1]:
         print("Question" + str(count) + ": Wrong")
     
+# Computations for grade precentage
 correctAnswers = round((int(correctAnswers) / int(questions)) * 100)
 print(name + " got " + str(correctAnswers) + "% / 100%")
 
